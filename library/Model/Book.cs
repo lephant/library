5ef2fc70253e55992f2b3245bbc3f1f7f0bc4362
lephant <levb97@yandex.ru>
2017-06-17 18:26:38 +0300

    Добавлен класс книги

---
+++ library/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace library.Model
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public Author Author { get; set; }
+        public Publisher Publisher { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int CountOfPages { get; set; }
+        public Place Place { get; set; }
+        public UsageCategory UsageCategory { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
