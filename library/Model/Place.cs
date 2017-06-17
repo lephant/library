2533ce488ee4ebd53063582f6fd05819e20769a9
lephant <levb97@yandex.ru>
2017-06-17 18:10:36 +0300

    Добавлен класс места хранения

---
+++ library/Model/Place.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace library.Model
+{
+    public class Place
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+    }
+}
