08fd920dabc938b0c5e760f1f16ed9fb00787c87
lephant <levb97@yandex.ru>
2017-06-17 18:09:26 +0300

    Добавлен класс автора

---
+++ library/Model/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace library.Model
+{
+    public class Author
+    {
+        public long id { get; set; }
+        public string fio { get; set; }
+    }
+}
