7c3ed0d17e028c13fdf2eb7667d8893afe189298
lephant <levb97@yandex.ru>
2017-06-17 18:26:04 +0300

    Добавлен класс издательства

---
+++ library/Model/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace library.Model
+{
+    public class Publisher
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
