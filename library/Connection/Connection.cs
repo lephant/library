68b2f8d9d1dbf6dccee75cd08d23bc0bb093ec3a
lephant <levb97@yandex.ru>
2017-06-17 23:53:08 +0300

    Добавлен класс соединения с бд

---
+++ library/Connection/Connection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace library.Connection
+{
+    public class Connection
+    {
+        private static MySqlConnection _sqlConnection;
+
+        private Connection()
+        {
+        }
+
+        public static MySqlConnection GetConnection()
+        {
+            if (_sqlConnection == null)
+            {
+                _sqlConnection = new MySqlConnection();
+            }
+            return _sqlConnection;
+        }
+    }
+}
