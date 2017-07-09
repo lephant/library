c69cdd94f695d9b6c94810988067ea115a2bb174
lephant <levb97@yandex.ru>
2017-07-09 10:49:59 +0300

    Добавлена логика для получения индекса записи, для которой вызвалось контекстное меню

--- library/Controls/ScrollableListBox.cs
+++ library/Controls/ScrollableListBox.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace library.Controls
@@ -6,6 +7,8 @@ namespace library.Controls
     public class ScrollableListBox : ListBox
     {
         private bool mouseOnControl;
+
+        public int ContextMenuItemIndex { get; private set; }
 
         protected override void OnMouseEnter(EventArgs e)
         {
@@ -29,6 +32,19 @@ namespace library.Controls
                 return;
             }
             base.WndProc(ref m);
+        }
+
+        protected override void OnContextMenuChanged(EventArgs e)
+        {
+            base.OnContextMenuChanged(e);
+            ContextMenu.Popup += ContextMenuOnPopup;
+        }
+
+        private void ContextMenuOnPopup(object sender, EventArgs e)
+        {
+            Point controlLocation = PointToScreen(new Point(0, 0));
+            Point clickLocation = Point.Subtract(Cursor.Position, (Size) controlLocation);
+            ContextMenuItemIndex = this.IndexFromPoint(clickLocation);
         }
     }
 }
